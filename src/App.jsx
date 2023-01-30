0f38d723d678176d0c4f3d9fbd0689861577ed70
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-30 22:15:44 +0330

    feat:Start implement logic program

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 
-import './App.css'
-
-// const url ='https://course-api.com/react-tours-project'
+const url ='https://course-api.com/react-tours-project'
 
 function App() {
-  // const [loading,setLoading] = useState(true)
-  // const [tours,setTours] = useState([])
-  // const response = fetch(url)
+
+  const [loading,setLoading] = useState(true)
+  const [tours,setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    }catch (error){
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+
+
   return (
     <div>
-      hi
+
     </div>
   )
 }
