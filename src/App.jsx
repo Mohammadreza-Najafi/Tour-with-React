6c2b6b0dce35617185acea20dc11e48a900e9e57
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-30 19:35:42 +0330

    first station

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+// const url ='https://course-api.com/react-tours-project'
+
+function App() {
+  // const [loading,setLoading] = useState(true)
+  // const [tours,setTours] = useState([])
+  // const response = fetch(url)
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App
