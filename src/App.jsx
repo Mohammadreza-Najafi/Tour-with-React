9e788b3f1c0f627a2b6a671eb3ed892254a54457
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-31 00:19:30 +0330

    feat:Add code for fetching tours and removing tours functionality in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {useEffect, useState} from 'react'
+import Tours from "./component/Tours.jsx";
+import Loading from "./component/Loading.jsx";
 
 const url ='https://course-api.com/react-tours-project'
 
@@ -16,17 +18,48 @@ function App() {
       setTours(tours)
     }catch (error){
       setLoading(false)
-      console.log(error)
+      // console.log(error)
     }
+  }
+
+  useEffect(() =>{
+    fetchTours()
+  },[])
+
+  if(loading){
+    return (
+        <main>
+          <Loading/>
+        </main>
+    )
+  }
+
+  const removeTour = (id)=>{
+    const newTours = tours.filter((tour)=>tour.id !== id)
+    setTours(newTours)
   }
 
 
 
-  return (
-    <div>
+  if (tours.length === 0){
+    return (
+        <main>
+          <div className="title">
+            <h2>no tours left</h2>
+            <button className="btn" onClick={() => fetchTours()}>
+              refresh
+            </button>
+          </div>
+        </main>
+    )
+  }
 
-    </div>
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
   )
+
 }
 
 export default App
